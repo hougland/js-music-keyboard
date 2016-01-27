8c8e8128819b2125071c90bfae2cbe578b48e11d
Ricky Hougland <houglande@gmail.com>
2016-01-27 15:48:08 -0800

    messed with a bunch of stuff then restored what I had before ha ha

--- noise.js
+++ noise.js
@@ -1,66 +1,39 @@
 $(document).ready( function() {
-  var cButton = $(".c"), cAudio = $("#cAudio"), dButton = $(".d"), dAudio = $("#dAudio"), eButton = $(".e"), eAudio = $("#eAudio"), fButton = $(".f"), fAudio = $("#fAudio"), gButton = $(".g"), gAudio = $("#gAudio"), aButton = $(".a"), aAudio = $("#aAudio"), bButton = $(".b"), bAudio = $("#bAudio");
   var playSound = function(selector) {
     selector.get(0).play();
   };
-  // var clickPlay = function() {
-  //   selector.click(function() {
-  //
-  //   });
-  // };
 
-  cButton.click(function() {
-    playSound(cAudio);
-  });
+  var notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
-  dButton.click(function() {
-    playSound(dAudio);
-  });
-
-  eButton.click(function() {
-    playSound(eAudio);
-  });
-
-  fButton.click(function() {
-    playSound(fAudio);
-  });
-
-  gButton.click(function() {
-    playSound(gAudio);
-  });
-
-  aButton.click(function() {
-    playSound(aAudio);
-  });
-
-  bButton.click(function() {
-    playSound(bAudio);
+  notes.forEach( function(note) {
+    $('.' + note).click( function() {
+      playSound($('#' + note + "Audio"));
+    });
   });
 
   $(document).bind('keydown', function(e) {
     switch(e.which) {
       case 67:
-        playSound(cAudio);
+        playSound($('#cAudio'));
         break;
       case 68:
-        playSound(dAudio);
+        playSound($('#dAudio'));
         break;
       case 69:
-        playSound(eAudio);
+        playSound($('#eAudio'));
         break;
       case 70:
-        playSound(fAudio);
+        playSound($('#fAudio'));
         break;
       case 71:
-        playSound(gAudio);
+        playSound($('#gAudio'));
         break;
       case 65:
-        playSound(aAudio);
+        playSound($('#aAudio'));
         break;
       case 66:
-        playSound(bAudio);
+        playSound($('#bAudio'));
         break;
     }
   });
-
 });
