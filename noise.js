c2ebcfe20fde3aee6e346e7f97beb75caa4ffae2
Ricky Hougland <houglande@gmail.com>
2016-01-27 14:21:06 -0800

    on click or keypress, will play correct sound

--- noise.js
+++ noise.js
@@ -1,3 +1,51 @@
 $(document).ready( function() {
-  // your code here
+  var cButton = $(".c"), cAudio = $("#cAudio"), dButton = $(".d"), dAudio = $("#dAudio"), eButton = $(".e"), eAudio = $("#eAudio"), fButton = $(".f"), fAudio = $("#fAudio"), gButton = $(".g"), gAudio = $("#gAudio"), aButton = $(".a"), aAudio = $("#aAudio"), bButton = $(".b"), bAudio = $("#bAudio");
+
+  cButton.click(function() {
+    cAudio.get(0).play();
+  });
+
+  dButton.click(function() {
+    dAudio.get(0).play();
+  });
+
+  eButton.click(function() {
+    eAudio.get(0).play();
+  });
+
+  fButton.click(function() {
+    fAudio.get(0).play();
+  });
+
+  gButton.click(function() {
+    gAudio.get(0).play();
+  });
+
+  aButton.click(function() {
+    aAudio.get(0).play();
+  });
+
+  bButton.click(function() {
+    bAudio.get(0).play();
+  });
+
+  $(document).bind('keydown', function(e) {
+    // letter "c"
+    if(e.keyCode == 67) {
+      cAudio.get(0).play();
+    } else if (e.keyCode == 68) {
+      dAudio.get(0).play();
+    } else if (e.keyCode == 69) {
+      eAudio.get(0).play();
+    } else if (e.keyCode == 70) {
+      fAudio.get(0).play();
+    } else if (e.keyCode == 71) {
+      gAudio.get(0).play();
+    } else if (e.keyCode == 65) {
+      aAudio.get(0).play();
+    } else if (e.keyCode == 66) {
+      bAudio.get(0).play();
+    }
+  });
+
 });
