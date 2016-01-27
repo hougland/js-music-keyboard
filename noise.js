01f7063048141ee3d19e01fe9e3038f3d35de07f
Ricky Hougland <houglande@gmail.com>
2016-01-27 14:33:56 -0800

    refactor by pulling out and defining play function

--- noise.js
+++ noise.js
@@ -1,50 +1,57 @@
 $(document).ready( function() {
   var cButton = $(".c"), cAudio = $("#cAudio"), dButton = $(".d"), dAudio = $("#dAudio"), eButton = $(".e"), eAudio = $("#eAudio"), fButton = $(".f"), fAudio = $("#fAudio"), gButton = $(".g"), gAudio = $("#gAudio"), aButton = $(".a"), aAudio = $("#aAudio"), bButton = $(".b"), bAudio = $("#bAudio");
+  var playSound = function(selector) {
+    selector.get(0).play();
+  };
+  // var clickPlay = function() {
+  //   selector.click(function() {
+  //
+  //   });
+  // };
 
   cButton.click(function() {
-    cAudio.get(0).play();
+    playSound(cAudio);
   });
 
   dButton.click(function() {
-    dAudio.get(0).play();
+    playSound(dAudio);
   });
 
   eButton.click(function() {
-    eAudio.get(0).play();
+    playSound(eAudio);
   });
 
   fButton.click(function() {
-    fAudio.get(0).play();
+    playSound(fAudio);
   });
 
   gButton.click(function() {
-    gAudio.get(0).play();
+    playSound(gAudio);
   });
 
   aButton.click(function() {
-    aAudio.get(0).play();
+    playSound(aAudio);
   });
 
   bButton.click(function() {
-    bAudio.get(0).play();
+    playSound(bAudio);
   });
 
   $(document).bind('keydown', function(e) {
-    // letter "c"
     if(e.keyCode == 67) {
-      cAudio.get(0).play();
-    } else if (e.keyCode == 68) {
-      dAudio.get(0).play();
-    } else if (e.keyCode == 69) {
-      eAudio.get(0).play();
-    } else if (e.keyCode == 70) {
-      fAudio.get(0).play();
-    } else if (e.keyCode == 71) {
-      gAudio.get(0).play();
-    } else if (e.keyCode == 65) {
-      aAudio.get(0).play();
-    } else if (e.keyCode == 66) {
-      bAudio.get(0).play();
+      playSound(cAudio);
+    } else if (e.which == 68) {
+      playSound(dAudio);
+    } else if (e.which == 69) {
+      playSound(eAudio);
+    } else if (e.which == 70) {
+      playSound(fAudio);
+    } else if (e.which == 71) {
+      playSound(gAudio);
+    } else if (e.which == 65) {
+      playSound(aAudio);
+    } else if (e.which == 66) {
+      playSound(bAudio);
     }
   });
 
